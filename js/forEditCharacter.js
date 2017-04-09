a3b71f277008d084d0550f0e1f5654dbe6f420ee
NishiyamaK <nkmonokuro3@gmail.com>
2017-04-09 23:35:13 +0900

    編集画面でキャラクターを削除できるように機能追加

--- js/forEditCharacter.js
+++ js/forEditCharacter.js
@@ -18,20 +18,48 @@ if (typeof localStorage == 'undefined') {
 }
 
 $(function(){
-  /*
-  既に作成されているキャラデータを読み込む
-  */
-  chara_dic = getFromLocalStrage("character");
-  console.log(chara_dic);
-  for(key in chara_dic){
-    var baseStatus = "";
-    for(i in chara_dic[key]["base"]){
-      baseStatus += "<td>" + chara_dic[key]["base"][i] + "</td>";
-    }
-    $('#ec_table1 tbody').append(
-      "<tr>" +
-        "<td>" + key + "</td>" + baseStatus +
-      "</tr>"
-    );
-  }
+  // キャラクター読み込み
+  readCharacters();
+
+  // ×ボタンが押された際に該当キャラを削除 ＆ 再読み込み
+  $('.delete-btn').click(function(){
+    console.log("ボタンが押された");
+    var c_name = $(this).attr('name'); //削除するキャラ名を取得
+    deleteCharacter(c_name);
+    location.reload();
+  });
 });
+
+function deleteCharacter(name){
+  /*
+  名前がnameのキャラクターを削除する
+  */
+  var chara_dic = getFromLocalStrage("character");
+  delete chara_dic[name];
+  setToLocalStrage("character", chara_dic);
+  console.log(name, "を削除しました.");
+};
+
+function readCharacters() {
+    /*
+    既に作成されているキャラデータを再度読み込む
+    */
+    //一度tableをクリアする
+    $('#ec_table1 tbody').empty();
+
+    // 読み込み
+    chara_dic = getFromLocalStrage("character");
+    for(name in chara_dic){
+      var baseStatus = "";
+      for(info in chara_dic[name]["base"]){
+        baseStatus += "<td>" + chara_dic[name]["base"][info] + "</td>";
+      }
+      $('#ec_table1 tbody').append(
+        "<tr>" +
+          "<td>" + name + `<a href="#" class="delete-btn" name="${name}">` +
+            '<i class="glyphicon glyphicon-remove"></a></button>' +
+          "</td>" + baseStatus +
+        "</tr>"
+      );
+    }
+}
